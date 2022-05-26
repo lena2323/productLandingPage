36be82108f905d2722a025538dd4f288aa934f8a
Lena2323 <lena.jovicic23@gmail.com>
2022-05-26 18:08:42 +0200

    js works. trying to make it look nice. failing.

--- script.js
+++ script.js
@@ -3,18 +3,32 @@ const subscribe = document.getElementById("subscribe");
 const subscribeContent = document.getElementById("subscribeContent");
 const aboutUs =  document.getElementById("aboutUs");
 const aboutUsContent = document.getElementById("aboutUsContent");
+const menu =  document.getElementById("menu");
+const menuContents = document.getElementById("menuContents");
 
-const navBarAddMarginBot = document.getElementById("nav-bar");
+
+
 
 subscribe.addEventListener('click',() =>{
     everythingToHide.classList.add('hide');
-    subscribeContent.classList.add('show');
+    subscribeContent.classList.remove('hide');
+    aboutUsContent.classList.add('hide');
+    menuContents.classList.add('hide');
 
   });
   
   aboutUs.addEventListener('click',() =>{
     everythingToHide.classList.add('hide');
     subscribeContent.classList.add('hide');
-    aboutUsContent.classList.add('show');
+    aboutUsContent.classList.remove('hide');
+    menuContents.classList.add('hide');
+
+});
+
+menu.addEventListener('click',() =>{
+  everythingToHide.classList.add('hide');
+  subscribeContent.classList.add('hide');
+  aboutUsContent.classList.add('hide');
+  menuContents.classList.remove('hide');
 
 });
