2269726569947fbba26d5631d5fec9334bc6af3c
Lena2323 <lena.jovicic23@gmail.com>
2022-07-27 13:57:44 +0200

    My first scss, i am a thief! A filthy thief! Looks cool tho.

--- script.js
+++ script.js
@@ -358,3 +358,19 @@ hamburger.addEventListener('click',() =>{
   navBar.classList.toggle('show');
 });
 
+
+
+
+/***************** Banner ****************/
+
+const background = document.querySelector('.background');
+
+document.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    
+    if(scrollY !== 0) {
+        background.style.backgroundPosition = `calc(50% + ${scrollY}px) calc(50% + ${scrollY}px)`;
+    } else {
+        background.style.backgroundPosition = '';
+    }
+});
